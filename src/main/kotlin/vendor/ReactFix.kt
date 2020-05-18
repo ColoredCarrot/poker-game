bc5698653e3f7353d29bb8a7c22ffc0d88788b17
Julian K <koch@in.tum.de>
2020-05-18 11:58:58 +0200

    ReactFix: alternative definitions of findDOMNode&friends adding @JsNonModule
    This fixes a compilation error using the standard definitions saying that they need both @JsModule and @JsNonModule annotations, but they only have @JsModule

---
+++ src/main/kotlin/vendor/ReactFix.kt
@@ -0,0 +1,19 @@
+@file:JsModule("react-dom")
+@file:JsNonModule
+
+package vendor
+
+import org.w3c.dom.Element
+import react.ReactElement
+
+// See https://reactjs.org/docs/react-dom.html
+
+external fun render(element: dynamic, container: Element?, callback: () -> Unit = definedExternally)
+
+external fun hydrate(element: dynamic, container: Element?, callback: () -> Unit = definedExternally)
+
+external fun unmountComponentAtNode(domContainerNode: Element?)
+
+external fun findDOMNode(component: dynamic): Element
+
+external fun createPortal(element: dynamic, container: Element?): ReactElement
