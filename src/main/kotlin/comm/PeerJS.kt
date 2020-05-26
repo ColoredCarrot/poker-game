3f51ad39027d87f82dc0d0dcc0aca9d4acf86386
Julian K <koch@in.tum.de>
2020-05-26 20:26:33 +0200

    Update PeerJS class

--- src/main/kotlin/comm/PeerJS.kt
+++ src/main/kotlin/comm/PeerJS.kt
@@ -5,13 +5,19 @@ import kotlinext.js.jsObject
 object PeerJS {
 
     fun createPeer(): dynamic {
-        val init = jsObject<dynamic> {
-            debug = 2
-            host = "localhost"
-            port = 9000
-            path = "/peer"
-        }
+        @Suppress("UNUSED_VARIABLE")
+        val init = PEERJS_CLOUD_INIT
         return js("new Peer(null, init)")
     }
 
+    private val LOCALHOST_INIT = jsObject<dynamic> {
+        debug = 2
+        host = "localhost"
+        port = 9000
+        path = "/peer"
+    }
+
+    private val PEERJS_CLOUD_INIT = jsObject<dynamic> {
+        debug = 2
+    }
 }
