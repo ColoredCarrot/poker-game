2c6de0b788709542ebf29784d18692e96336287a
Julian K <koch@in.tum.de>
2020-05-26 18:23:13 +0200

    Split welcome into two columns

--- src/main/kotlin/shared/WelcomeGamePhase.kt
+++ src/main/kotlin/shared/WelcomeGamePhase.kt
@@ -9,6 +9,7 @@ import react.RBuilder
 import react.RProps
 import react.child
 import react.dom.a
+import react.dom.button
 import react.dom.div
 import react.dom.h1
 import react.dom.input
@@ -32,29 +33,36 @@ private val WelcomeGamePhase = functionalComponentEx<WelcomeGamePhaseProps>("Wel
     lobbyContainer {
         h1 { +"Poker" }
 
-        p {
-            +"Enter a game ID and press Enter to join a game. Alternatively, you can also "
-            a(href = "#") {
-                +"host your own game"
-                attrs.onClickFunction = { evt ->
-                    evt.preventDefault()
-                    props.hostGameFn()
-                }
-            }
-            +"."
-        }
-
-        label {
-            +"Game ID:"
-            input(type = InputType.text) {
-                attrs.autoComplete = false
-                attrs.onKeyUpFunction = { evt ->
-                    if (evt.asDynamic().key as String == "Enter") {
-                        evt.preventDefault()
-                        props.enterGameFn((evt.currentTarget as HTMLInputElement).value)
+        div("uk-container") {
+            // Divide container into two columns
+            div("uk-width-1-2 uk-display-inline-block") {
+                div { +"Enter a game ID and press Enter to join a game." }
+                label {
+                    +"Game ID:"
+                    input(type = InputType.text) {
+                        attrs.autoComplete = false
+                        attrs.onKeyUpFunction = { evt ->
+                            // Note: we cannot actually cast to KeyboardEvent
+                            // because React provides its own Event with the
+                            // same interface, but whose type is not KeyboardEvent.
+                            if (evt.asDynamic().key as String == "Enter") {
+                                evt.preventDefault()
+                                props.enterGameFn((evt.currentTarget as HTMLInputElement).value)
+                            }
+                        }
                     }
                 }
-            }
-        }
+            } // first column
+            div("uk-width-1-2 uk-display-inline-block") {
+                div { +"Alternatively, you can also host your own game:" }
+                button(classes = "uk-button uk-button-large uk-button-secondary") {
+                    +"Host your own game"
+                    attrs.onClickFunction = { evt ->
+                        evt.preventDefault()
+                        props.hostGameFn()
+                    }
+                }
+            } // second column
+        } // two-split container
     }
 }
