e753894ec8409dfb716259be58c247d9b52b2ecb
Julian K <koch@in.tum.de>
2020-05-28 21:44:09 +0200

    Convert ParticipantLobbyGamePhase to class component, fixing captured state issue

--- src/main/kotlin/participant/ParticipantLobbyGamePhase.kt
+++ src/main/kotlin/participant/ParticipantLobbyGamePhase.kt
@@ -4,21 +4,18 @@ import comm.Participant
 import comm.msg.Messages
 import comm.msg.handledBy
 import comm.msg.send
-import kotlinext.js.jsObject
 import react.RBuilder
+import react.RComponent
 import react.RProps
-import react.child
+import react.RState
 import react.dom.div
 import react.dom.h1
 import react.dom.span
 import react.dom.strong
-import react.getValue
-import react.setValue
-import react.useEffectWithCleanup
-import react.useState
-import reactutils.functionalComponentEx
+import react.setState
 import shared.SessionId
 import shared.Table
+import shared.childEx
 import shared.htmlAttrs
 import usingreact.lobbyContainer
 import usingreact.lobbyPlayerList
@@ -27,64 +24,85 @@ fun RBuilder.participantLobbyGamePhase(
     connection: Participant,
     switchToPlayingPhaseFn: (initialTable: Table, firstAnte: Int, activePlayer: SessionId?) -> Unit
 ) =
-    child(ParticipantLobbyGamePhase, jsObject {
+    childEx(ParticipantLobbyGamePhase::class) {
         this.connection = connection
         this.switchToPlayingPhaseFn = switchToPlayingPhaseFn
-    })
+    }
 
 private external interface ParticipantLobbyGamePhaseProps : RProps {
     var connection: Participant
     var switchToPlayingPhaseFn: (initialTable: Table, firstAnte: Int, activePlayer: SessionId?) -> Unit
 }
 
-private val ParticipantLobbyGamePhase =
-    functionalComponentEx<ParticipantLobbyGamePhaseProps>("ParticipantLobbyGamePhase") { props ->
+private class ParticipantLobbyGamePhaseState : RState {
+    var connectedToHost = false
+    var error: dynamic = null
+    var myPlayerName = ""
+    var myPlayerNameValidated = false
+    var otherPlayerNames = listOf<String>()
+}
 
-        var connectedToHost by useState(false)
-        var error by useState<dynamic>(null)
-        var myPlayerName by useState("")
-        var myPlayerNameValidated by useState(false)
-        var otherPlayerNames by useState(listOf<String>())
+private class ParticipantLobbyGamePhase : RComponent<ParticipantLobbyGamePhaseProps, ParticipantLobbyGamePhaseState>() {
 
-        useEffectWithCleanup(listOf()) {
-            props.connection.receive(
-                Messages.Lobby_UpdatePlayerList.Type handledBy { (m) ->
-                    // TODO Fix: myPlayerName might be stale at the time the otherPlayerNames state update is executed
+    init {
+        state = ParticipantLobbyGamePhaseState()
+    }
+
+    override fun componentDidMount() {
+        props.connection.receive(
+            Messages.Lobby_UpdatePlayerList.Type handledBy { (m) ->
+                setState {
                     otherPlayerNames = m.allNames.filter { it != myPlayerName }
                     myPlayerNameValidated = myPlayerName in m.allNames
-                },
-                Messages.TotalGameReset.Type handledBy { (m) ->
-                    props.switchToPlayingPhaseFn(m.yourTable, m.ante, m.activePlayer)
                 }
-            )
-
-            props.connection.hook.connectedToPeer {
-                connectedToHost = true
+            },
+            Messages.TotalGameReset.Type handledBy { (m) ->
+                props.switchToPlayingPhaseFn(m.yourTable, m.ante, m.activePlayer)
             }
-            props.connection.hook.errorConnectingToPeer { error = it }
-            props.connection.hook.error { error = it }
+        )
 
-            return@useEffectWithCleanup {
-                props.connection.receive()
-                props.connection.hook.clear()
-            }
+        props.connection.hook.connectedToPeer {
+            setState { connectedToHost = true }
         }
+        props.connection.hook.errorConnectingToPeer { err -> setState { error = err } }
+        props.connection.hook.error { err -> setState { error = err } }
+    }
 
+    override fun componentWillUnmount() {
+        props.connection.receive()
+        props.connection.hook.clear()
+    }
+
+    override fun componentDidUpdate(
+        prevProps: ParticipantLobbyGamePhaseProps,
+        prevState: ParticipantLobbyGamePhaseState,
+        snapshot: Any
+    ) {
+        // Send update if we changed our name, and invalidate until we receive confirmation from server
+        if (prevState.myPlayerName != state.myPlayerName) {
+            setState { myPlayerNameValidated = false }
+            props.connection.send(Messages.Lobby_SetName(state.myPlayerName, props.connection.peerId))
+        }
+    }
+
+    override fun RBuilder.render() {
         lobbyContainer {
             h1 { +"Join Game" }
 
-            if (error != null) div("poker-lobby-error") {
-                strong { +"$error" }
+            if (state.error != null) div("poker-lobby-error") {
+                strong { +"${state.error}" }
             } else div {
                 +"Status: "
 
-                if (connectedToHost) {
+                if (state.connectedToHost) {
                     span("uk-text-success") { +"Connected. " }
                     +"Waiting for host to start game..."
-                    lobbyPlayerList(otherPlayerNames, myPlayerName, myPlayerNameValidated) { newName ->
-                        myPlayerName = newName
-                        props.connection.send(Messages.Lobby_SetName(newName, props.connection.peerId))
-                        myPlayerNameValidated = false
+                    lobbyPlayerList(
+                        state.otherPlayerNames,
+                        state.myPlayerName,
+                        state.myPlayerNameValidated
+                    ) { newName ->
+                        setState { myPlayerName = newName }
                     }
                 } else {
                     +"Connecting..."
@@ -95,3 +113,4 @@ private val ParticipantLobbyGamePhase =
             }
         }
     }
+}
