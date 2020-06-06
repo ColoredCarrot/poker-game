38c0dc0265cf40cfec7d1d90d8adbc5c50c22cef
Julian K <koch@in.tum.de>
2020-06-06 19:56:33 +0200

    Make ValueDisplay icon clickable to copy value to clipboard

--- src/main/kotlin/usingreact/ValueDisplay.kt
+++ src/main/kotlin/usingreact/ValueDisplay.kt
@@ -1,16 +1,19 @@
 package usingreact
 
 import kotlinx.html.InputType
+import kotlinx.html.js.onClickFunction
 import kotlinx.html.js.onFocusFunction
 import org.w3c.dom.HTMLInputElement
 import react.RBuilder
 import react.RProps
 import react.RPureComponent
 import react.RState
+import react.dom.a
 import react.dom.div
 import react.dom.input
-import react.dom.span
 import shared.htmlAttrs
+import vendor.createRef
+import kotlin.browser.document
 
 fun RBuilder.valueDisplay(value: String, autoFocus: Boolean = false) = child(ValueDisplay::class) {
     attrs {
@@ -30,18 +33,32 @@ private class ValueDisplayState : RState {
 
 private class ValueDisplay : RPureComponent<ValueDisplayProps, ValueDisplayState>() {
 
+    private val inputRef = createRef<HTMLInputElement>()
+
     override fun RBuilder.render() {
         div("uk-inline uk-width-1-1") {
-            span("uk-form-icon uk-form-icon-flip") {
+            a(classes = "uk-form-icon uk-form-icon-flip") {
                 htmlAttrs["uk-icon"] = "icon: copy"
+                attrs.onClickFunction = { evt ->
+                    evt.preventDefault()
+                    val inputEl = inputRef.current
+                    if (inputEl != null) {
+                        inputEl.focus()
+                        inputEl.select()
+                        document.execCommand("copy")
+                    }
+                }
             }
             input(classes = "uk-input", type = InputType.text) {
-                attrs.value = props.value
-                attrs.autoFocus = props.autoFocus
-                attrs.readonly = true
-                attrs.onFocusFunction = { evt ->
-                    val el = evt.currentTarget as? HTMLInputElement
-                    el?.select()
+                ref = inputRef
+                attrs {
+                    value = props.value
+                    autoFocus = props.autoFocus
+                    readonly = true
+                    onFocusFunction = { evt ->
+                        val el = evt.currentTarget as? HTMLInputElement
+                        el?.select()
+                    }
                 }
             }
         }
